d4d587833ba8244ab01ef4b803edf64a3f0bc787
Neil-Ni <tzuyangni@gmail.com>
2024-01-28 14:21:39 -0500

    adjust default data

--- src/models/timeslot.test.ts
+++ src/models/timeslot.test.ts
@@ -4,7 +4,7 @@ import { allKeys } from './utils';
 
 describe('timeslot', () => {
   it('matches the shape of the default data', () => {
-    const days: Timeslot[] = data['schedule']['2016-09-09']['timeslots'];
+    const days: Timeslot[] = data['schedule']['2024-08-07']['timeslots'];
     const keys: Array<keyof Timeslot> = ['endTime', 'sessions', 'startTime'];
     expect(days).toHaveLength(13);
     expect(allKeys(days)).toStrictEqual(keys);
--- src/models/track.test.ts
+++ src/models/track.test.ts
@@ -4,7 +4,7 @@ import { allKeys } from './utils';
 
 describe('track', () => {
   it('matches the shape of the default data', () => {
-    const days: Track[] = data['schedule']['2016-09-09']['tracks'];
+    const days: Track[] = data['schedule']['2024-08-07']['tracks'];
     const keys: Array<keyof Track> = ['title'];
     expect(days).toHaveLength(3);
     expect(allKeys(days)).toStrictEqual(keys);
