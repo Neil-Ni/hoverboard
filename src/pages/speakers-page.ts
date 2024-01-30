fca64a63c4090d8b0722c4745ca7e9687f54ce83
Neil-Ni <tzuyangni@gmail.com>
2024-01-29 22:16:48 -0500

    hide speakers

--- src/pages/speakers-page.ts
+++ src/pages/speakers-page.ts
@@ -184,75 +184,81 @@ export class SpeakersPage extends ReduxMixin(PolymerElement) {
         results-count="[[speakersToRender.length]]"
       ></filter-menu>
 
-      <content-loader
-        class="container"
-        card-padding="32px"
-        card-height="400px"
-        avatar-size="128px"
-        avatar-circle="64px"
-        horizontal-position="50%"
-        border-radius="4px"
-        box-shadow="var(--box-shadow)"
-        items-count="[[contentLoaders.speakers.itemsCount]]"
-        hidden$="[[contentLoaderVisibility]]"
-      ></content-loader>
+      <div layout horizontal center-justified style="margin-top: 50px">
+        <a href="https://5dvfzy5ysgo.typeform.com/to/vxNaK6Hr" rel="noopener noreferrer">
+          <paper-button primary>Apply to be an exhibitor</paper-button>
+        </a>
+      </div>
+
+<!--      <content-loader-->
+<!--        class="container"-->
+<!--        card-padding="32px"-->
+<!--        card-height="400px"-->
+<!--        avatar-size="128px"-->
+<!--        avatar-circle="64px"-->
+<!--        horizontal-position="50%"-->
+<!--        border-radius="4px"-->
+<!--        box-shadow="var(&#45;&#45;box-shadow)"-->
+<!--        items-count="[[contentLoaders.speakers.itemsCount]]"-->
+<!--        hidden$="[[contentLoaderVisibility]]"-->
+<!--      ></content-loader>-->
 <!--      TODO: Add apply form right here -->
 
-      <div class="container">
-        <template is="dom-repeat" items="[[speakersToRender]]" as="speaker">
-          <a class="speaker card" href$="[[speakerUrl(speaker.id)]]">
-            <div relative>
-              <lazy-image
-                class="photo"
-                src="[[speaker.photoUrl]]"
-                alt="[[speaker.name]]"
-              ></lazy-image>
-              <div class="badges" layout horizontal>
-                <template is="dom-repeat" items="[[speaker.badges]]" as="badge">
-                  <a
-                    class$="badge [[badge.name]]-b"
-                    href$="[[badge.link]]"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    title$="[[badge.description]]"
-                    layout
-                    horizontal
-                    center-center
-                  >
-                    <iron-icon icon="hoverboard:[[badge.name]]" class="badge-icon"></iron-icon>
-                  </a>
-                </template>
-              </div>
-            </div>
+<!--      <div class="container">-->
+<!--        <template is="dom-repeat" items="[[speakersToRender]]" as="speaker">-->
+<!--          <a class="speaker card" href$="[[speakerUrl(speaker.id)]]">-->
+<!--            <div relative>-->
+<!--              <lazy-image-->
+<!--                class="photo"-->
+<!--                src="[[speaker.photoUrl]]"-->
+<!--                alt="[[speaker.name]]"-->
+<!--              ></lazy-image>-->
+<!--              <div class="badges" layout horizontal>-->
+<!--                <template is="dom-repeat" items="[[speaker.badges]]" as="badge">-->
+<!--                  <a-->
+<!--                    class$="badge [[badge.name]]-b"-->
+<!--                    href$="[[badge.link]]"-->
+<!--                    target="_blank"-->
+<!--                    rel="noopener noreferrer"-->
+<!--                    title$="[[badge.description]]"-->
+<!--                    layout-->
+<!--                    horizontal-->
+<!--                    center-center-->
+<!--                  >-->
+<!--                    <iron-icon icon="hoverboard:[[badge.name]]" class="badge-icon"></iron-icon>-->
+<!--                  </a>-->
+<!--                </template>-->
+<!--              </div>-->
+<!--            </div>-->
 
-            <lazy-image
-              class="company-logo"
-              src="[[speaker.companyLogoUrl]]"
-              alt="[[speaker.company]]"
-            ></lazy-image>
+<!--            <lazy-image-->
+<!--              class="company-logo"-->
+<!--              src="[[speaker.companyLogoUrl]]"-->
+<!--              alt="[[speaker.company]]"-->
+<!--            ></lazy-image>-->
 
-            <div class="description">
-              <h2 class="name">[[speaker.name]]</h2>
-              <div class="origin">[[speaker.country]]</div>
+<!--            <div class="description">-->
+<!--              <h2 class="name">[[speaker.name]]</h2>-->
+<!--              <div class="origin">[[speaker.country]]</div>-->
 
-              <text-truncate lines="5">
-                <div class="bio">[[speaker.bio]]</div>
-              </text-truncate>
-            </div>
+<!--              <text-truncate lines="5">-->
+<!--                <div class="bio">[[speaker.bio]]</div>-->
+<!--              </text-truncate>-->
+<!--            </div>-->
 
-            <div class="contacts">
-              <template is="dom-repeat" items="[[speaker.socials]]" as="social">
-                <a href$="[[social.link]]" target="_blank" rel="noopener noreferrer">
-                  <paper-icon-button
-                    class="social-icon"
-                    icon="hoverboard:{{social.icon}}"
-                  ></paper-icon-button>
-                </a>
-              </template>
-            </div>
-          </a>
-        </template>
-      </div>
+<!--            <div class="contacts">-->
+<!--              <template is="dom-repeat" items="[[speaker.socials]]" as="social">-->
+<!--                <a href$="[[social.link]]" target="_blank" rel="noopener noreferrer">-->
+<!--                  <paper-icon-button-->
+<!--                    class="social-icon"-->
+<!--                    icon="hoverboard:{{social.icon}}"-->
+<!--                  ></paper-icon-button>-->
+<!--                </a>-->
+<!--              </template>-->
+<!--            </div>-->
+<!--          </a>-->
+<!--        </template>-->
+<!--      </div>-->
 
 <!--      <previous-speakers-block></previous-speakers-block>-->
 
